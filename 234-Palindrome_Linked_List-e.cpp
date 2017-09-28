ae2679b1bf2367be471e528b870c4311cb9ea131
ysfree <tshu92@gmail.com>
2017-09-28 14:50:15 -0700

    update 234

--- 234-Palindrome_Linked_List-e.cpp
+++ 234-Palindrome_Linked_List-e.cpp
@@ -10,9 +10,32 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+// just follow regular reverse list method
+// ref 206
 class Solution {
+    ListNode *reverseList(ListNode *head) {
+        ListNode *temp = NULL;
+        while (head) {
+            std::swap(temp, head->next);
+            std::swap(head, temp);
+        }
+        return temp;
+    }
+
   public:
     bool isPalindrome(ListNode *head) {
-        
+        ListNode *slow = head, *fast = head;
+        while (fast && fast->next) {
+            fast = fast->next->next;
+            slow = slow->next;
+        }
+        slow = reverseList(slow);
+        while (slow) {
+            if (slow->val != head->val)
+                return 0;
+            slow = slow->next;
+            head = head->next;
+        }
+        return 1;
     }
 };
