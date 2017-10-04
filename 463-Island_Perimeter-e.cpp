e0a9eb5b68b979a82acea394a19b2915482ae977
ysfree <tshu92@gmail.com>
2017-10-04 14:46:40 -0700

    update 463

--- 463-Island_Perimeter-e.cpp
+++ 463-Island_Perimeter-e.cpp
@@ -16,56 +16,89 @@
 class Solution {
   public:
     int islandPerimeter(vector<vector<int>> &grid) {
-        int height = grid.size(), width = height ? grid[0].size();
+        int height = grid.size(), width = height ? grid[0].size() : 0;
         if (!(height && width))
             return 0;
 
         int result = 0;
         int i, j, startx, starty;
-        for (i = 0; i < height; ++i)
-            for (j = 0; j < width; ++j)
-                if (grid[i][j]) {
-                    startx = i;
-                    starty = j;
-                    break;
-                    break;
-                }
+        [&] {
+            for (i = 0; i < height; ++i)
+                for (j = 0; j < width; ++j)
+                    if (grid[i][j]) {
+                        startx = --i;
+                        starty = --j;
+                        return; // break 2 loops
+                    }
+        }();
 
         char dir = 'r'; // direction left, right, up, down
         do {
             switch (dir) {
             case 'r':
-                while (j < width && grid[i][j]) {
+                ++j;
+                while (j < width && grid[i + 1][j] &&
+                       (i > -1 ? !grid[i][j] : 1)) {
                     ++result;
                     ++j;
                 }
-                dir = 'd';
-                --j;
+                if (j == width || !grid[i + 1][j]) {
+                    dir = 'd';
+                } else {
+                    dir = 'u';
+                    ++i;
+                    --j;
+                }
                 break;
 
             case 'd':
-                while (i < height && grid[i][j] &&
-                       (j < width - 1 ? !grid[i][j + 1] : 1)) {
+                ++i;
+                while (i < height && grid[i][j - 1] &&
+                       (j < width ? !grid[i][j] : 1)) {
                     ++result;
                     ++i;
                 }
-                if(i == height || !grid[i][j])
+                if (i == height || !grid[i][j - 1]) {
                     dir = 'l';
-                    else 
+                } else {
                     dir = 'r';
-                --i;
+                    --i;
+                    --j;
+                }
                 break;
 
             case 'l':
+                --j;
+                while (j > -1 && grid[i - 1][j] &&
+                       (i < height ? !grid[i][j] : 1)) {
+                    ++result;
+                    --j;
+                }
+                if (j == -1 || !grid[i - 1][j]) {
+                    dir = 'u';
+                } else {
+                    dir = 'd';
+                    --i;
+                    ++j;
+                }
                 break;
 
             case 'u':
-                while (j < width && grid[i][j]) {
+                --i;
+                while (i > -1 && grid[i][j + 1] && (j > -1 ? !grid[i][j] : 1)) {
                     ++result;
+                    --i;
+                }
+                if (i == -1 || !grid[i][j + 1]) {
+                    dir = 'r';
+                } else {
+                    dir = 'l';
+                    ++i;
                     ++j;
                 }
                 break;
             }
         } while (i != startx || j != starty);
+        return result;
     }
 };
