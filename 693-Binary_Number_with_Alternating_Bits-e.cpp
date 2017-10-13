52fcce6c2fb4843fae65fc12ff5cf3a0691d59f3
ysfree <tshu92@gmail.com>
2017-10-13 13:57:26 -0700

    update 693

--- 693-Binary_Number_with_Alternating_Bits-e.cpp
+++ 693-Binary_Number_with_Alternating_Bits-e.cpp
@@ -1,15 +1,35 @@
-
-
+// Given a positive integer, check whether it has alternating bits: namely, if
+// two adjacent bits will always have different values.
+// Example 1:
+// Input: 5
+// Output: True
+// Explanation:
+// The binary representation of 5 is: 101
+// Example 2:
+// Input: 7
+// Output: False
+// Explanation:
+// The binary representation of 7 is: 111.
+// Example 3:
+// Input: 11
+// Output: False
+// Explanation:
+// The binary representation of 11 is: 1011.
+// Example 4:
+// Input: 10
+// Output: True
+// Explanation:
+// The binary representation of 10 is: 1010.
 
 class Solution {
-public:
+  public:
     bool hasAlternatingBits(int n) {
         int temp = 0xffffffff;
         int i = 0;
         while (n >> ++i)
             ;
         temp <<= i;
-         
+
         return (temp ^ (n ^ n >> 1)) == 0xffffffff;
     }
 };
