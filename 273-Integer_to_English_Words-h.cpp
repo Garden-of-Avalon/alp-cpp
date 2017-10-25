4198626e194271de206d31a34bc8951e87bdfbac
ysfree <tshu92@gmail.com>
2017-10-24 17:00:04 -0700

    update 273

--- 273-Integer_to_English_Words-h.cpp
+++ 273-Integer_to_English_Words-h.cpp
@@ -7,18 +7,62 @@
 // Seven"
 
 class Solution {
+    string smallNumToWords(int num) {
+        vector<string> less20 = {"One",       "Two",      "Three",   "Four",
+                                 "Five",      "Six",      "Seven",   "Eight",
+                                 "Nine",      "Ten",      "Eleven",  "Twelve",
+                                 "Thirteen",  "Fourteen", "Fifteen", "Sixteen",
+                                 "Seventeen", "Eighteen", "Nineteen"};
+        vector<string> tens = {"Twenty",  "Thirty", "Forty",  "Fifty",  "Sixty",
+                               "Seventy", "Eighty", "Ninety"};
+        int t = 0;
+        string result;
+        // assume 123
+        if ((t = num / 100) > 0) {
+            // t = 1
+            result += less20[t - 1] + " Hundred";
+        }
+        if ((t = num % 100) > 19) {
+            if (!result.empty())
+                result += " ";
+            // t = 23, 23 / 10 - 2 = 0
+            result += tens[t / 10 - 2];
+            if (t % 10 > 0)
+                result += " " + less20[t % 10 - 1];
+        } else if ((t = num % 20) > 0) {
+            if (!result.empty())
+                result += " ";
+            // t = 3
+            result += less20[t - 1];
+        }
+        return result;
+    }
+
   public:
     string numberToWords(int num) {
-        std::array<int> vals = {1e9, 1e6, 1e3, 100, 90, 80, 70, 60, 50, 40, 30,
-                                20,  19,  18,  17,  16, 15, 14, 13, 12, 11, 10,
-                                9,   8,   7,   6,   5,  4,  3,  2,  1};
-        std::array<string> words = {
-            "Billion", "Million", "Thousand", "Hundred",  "Ninety",
-            "Eighty",  "Seventy", "Sixty",    "Fifty",    "Forty",
-            "Thirty",  "Twenty",  "Nineteen", "Eighteen", "Seventeen",
-            "Sixteen", "Fifteen", "Fourteen", "Thirteen", "Twelve",
-            "Eleven",  "Ten",     "Nine",     "Eight",    "Seven",
-            "Six",     "Five",    "Four",     "Three",    "Two",
-            "One"};
+        if (!num)
+            return "Zero";
+
+        string result;
+        if (num >= 1000000000)
+            result += smallNumToWords(num / 1000000000) + " Billion";
+        num %= 1000000000;
+        if (num >= 1000000) {
+            if (!result.empty())
+                result += " ";
+            result += smallNumToWords(num / 1000000) + " Million";
+        }
+        num %= 1000000;
+        if (num  >= 1000) {
+            if (!result.empty())
+                result += " ";
+            result += smallNumToWords(num / 1000) + " Thousand";
+        }
+        num %= 1000;
+        if (!result.empty() && num)
+            result += " ";
+        result += smallNumToWords(num);
+
+        return result;
     }
 };
