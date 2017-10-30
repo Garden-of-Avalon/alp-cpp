2d656e2dccc81932e785eb0d9e55403b925441f7
ysfree <tshu92@gmail.com>
2017-10-29 22:41:15 -0700

    update 069

---
+++ 069-Sqrt(x)-e.cpp
@@ -0,0 +1,13 @@
+// Implement int sqrt(int x).
+// Compute and return the square root of x.
+
+// https://en.wikipedia.org/wiki/Integer_square_root#Using_only_integer_division
+class Solution {
+  public:
+    int mySqrt(int x) {
+        long r = x;
+        while (r * r > x)
+            r = (r + x / r) >> 1;
+        return r;
+    }
+};
