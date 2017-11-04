60a5871d25013bc8a515b0850cf5cefc010eb119
ysfree <tshu92@gmail.com>
2017-11-04 00:15:05 -0700

    update 326

---
+++ 326-Power_of_Three-e.cpp
@@ -0,0 +1,18 @@
+// Given an integer, write a function to determine if it is a power of three.
+// Follow up:
+// Could you do it without using any loop / recursion?
+// Credits:
+// Special thanks to @dietpepsi for adding this problem and creating all test
+// cases.
+
+class Solution {
+  public:
+    // use log or log2 may lead to wrong answer due to accuracy
+    bool isPowerOfThree(int n) { return fmod(log10(n) / log10(3), 1) == 0; }
+};
+
+class Solution {
+  public:
+    // 3^19
+    bool IsPowerOfThree(int n) { return n > 0 && (1162261467 % n == 0); }
+}
