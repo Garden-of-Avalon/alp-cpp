b7a5868aa95258498cd3baca2a42530cf136c168
ysfree <tshu92@gmail.com>
2017-11-03 20:37:53 -0700

    update 479

--- 479-Largest_Palindrome_Product-e.cpp
+++ 479-Largest_Palindrome_Product-e.cpp
@@ -20,12 +20,42 @@
 // 10: 9999999999 x 9999900001 = 99999000000000099999
 
 class Solution {
+    // long buildpl(const int &half) {
+    //     string halfstr = to_string(half);
+    //     string rightstr(halfstr.rbegin(), halfstr.rend());
+    //     return stol(halfstr + rightstr);
+    // }
+
+    long buildpl(int half) {
+        long pl = half;
+        while (half) {
+            pl = pl * 10 + half % 10;
+            half /= 10;
+        }
+        return pl;
+    }
+
   public:
     int largestPalindrome(int n) {
-        long x = pow(10, n + 1) - 1, y;
-        if (!n % 2) {
-            y = x - pow(10, n / 2 + 1) + 2;
-            return x * y % 1337;
+        if (n == 1)
+            return 9;
+
+        int x = pow(10, n) - 1, y;
+
+        if (!(n % 2)) {
+            y = x - pow(10, n / 2) + 2;
+            return static_cast<long>(x) * y % 1337;
         }
+
+        int low = pow(10, n - 1);
+        for (int half = x - 1; half > low; --half) {
+            long pl = buildpl(half);
+            int sqrtpl = sqrt(pl);
+            for (y = x; y >= sqrtpl; --y)
+                if (!(pl % y))
+                    return pl % 1337;
+        }
+
+        return 0;
     }
 };
