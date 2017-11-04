9b01fe6f9e08568809ec737f146639c4cf04249c
ysfree <tshu92@gmail.com>
2017-11-04 00:17:17 -0700

    326

--- 326-Power_of_Three-e.cpp
+++ 326-Power_of_Three-e.cpp
@@ -8,7 +8,9 @@
 class Solution {
   public:
     // use log or log2 may lead to wrong answer due to accuracy
-    bool isPowerOfThree(int n) { return fmod(log10(n) / log10(3), 1) == 0; }
+    bool isPowerOfThree(int n) {
+        return n > 0 && fmod(log10(n) / log10(3), 1) == 0;
+    }
 };
 
 class Solution {
