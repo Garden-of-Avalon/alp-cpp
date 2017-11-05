8f8c0c95c2c9f992fb19c9d324b79557ef2e4788
ysfree <tshu92@gmail.com>
2017-11-04 17:45:05 -0700

    update to new version

--- 233-Number_of_Digit_One-h.cpp
+++ 233-Number_of_Digit_One-h.cpp
@@ -11,28 +11,23 @@ class Solution {
         if (n < 0)
             return 0;
 
-        vector<int> steps;
-        for (int i = 1; i <= n; i *= 10) {
-            steps.push_back(i);
-            if (INT_MAX / i < 10)
-                break;
-        }
-
         int cnt = 0;
 
-        for (auto riter = steps.rbegin(); riter != steps.rend(); ++riter) {
+        for (int i = 1; i <= n; i *= 10) {
             // at each loop, add contribution of this digit
             // e.g. for 1124
-            // 1st loop, we add 125 for 1000 digit contribution
-            // 2nd loop, we add 25 + 100 (100-199) for 100 digit contribution
-            // 3rd loop, we add 10 + 10 * (n/100) (010-019, 110-119...1010-1019)
-            // 4th loop, we add 1 + 1 * (n/10) (0001, 0011, 0021...1111)
-            int digit = (n / *riter) % 10;
+            // 1st loop, we add 1 + 1 * (n/10) (0001, 0011, 0021...1111)
+            // 2nd loop, we add 10 + 10 * (n/100) (010-019, 110-119...1010-1019)
+            // 3rd loop, we add 25 + 100 (100-199) for 100 digit contribution
+            // 4th loop, we add 125 for 1000 digit contribution
+            int digit = (n / i) % 10;
             if (digit == 1)
-                cnt += n % *riter + 1;
+                cnt += n % i + 1;
             else if (digit > 1)
-                cnt += *riter;
-            cnt += *riter * (n / *riter / 10);
+                cnt += i;
+            cnt += i * (n / i / 10);
+            if (INT_MAX / i < 10)
+                break;
         }
 
         return cnt;
