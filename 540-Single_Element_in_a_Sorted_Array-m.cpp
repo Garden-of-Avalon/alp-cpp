10fe72d33cb4b46b3916b7cace3bf933fbbc1151
ysfree <tshu92@gmail.com>
2017-11-09 00:32:52 -0800

    update 540

--- 540-Single_Element_in_a_Sorted_Array-m.cpp
+++ 540-Single_Element_in_a_Sorted_Array-m.cpp
@@ -8,3 +8,37 @@
 // Input: [3,3,7,7,10,11,11]
 // Output: 10
 // Note: Your solution should run in O(log n) time and O(1) space.
+
+class Solution {
+  public:
+    int singleNonDuplicate(vector<int> &nums) {
+        if (nums.empty())
+            return 0;
+
+        int l = 0, r = nums.size() - 1, mid;
+
+        while (l < r) {
+            mid = (l + r) / 2;
+
+            // mid is odd, first half is even
+            // if (mid % 2) {
+            //     if (nums[mid] == nums[mid - 1])
+            //         l = mid + 1;
+            //     else
+            //         r = mid - 1;
+            // } else {
+            //     if (nums[mid] == nums[mid + 1])
+            //         l = mid + 1;
+            //     else
+            //         r = mid - 1;
+            // }
+
+            if (nums[mid] == nums[mid ^ 1])
+                l = mid + 1;
+            else
+                r = mid - 1;
+        }
+
+        return nums[l];
+    }
+};
