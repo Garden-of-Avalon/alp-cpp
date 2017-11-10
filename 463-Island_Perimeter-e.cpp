8f6ba334cff28df6d4fedde18df563798a911fd3
ysfree <tshu92@gmail.com>
2017-11-10 11:14:57 -0800

    update 463 reference

--- 463-Island_Perimeter-e.cpp
+++ 463-Island_Perimeter-e.cpp
@@ -13,6 +13,7 @@
 //  [1,1,0,0]]
 // Answer: 16
 
+// Too long
 class Solution {
   public:
     int islandPerimeter(vector<vector<int>> &grid) {
@@ -102,3 +103,31 @@ class Solution {
         return result;
     }
 };
+
+// Ref C++
+// Count islands and neighbors
+// int islandPerimeter(vector<vector<int>> &grid) {
+//     int count = 0, repeat = 0;
+//     for (int i = 0; i < grid.size(); i++) {
+//         for (int j = 0; j < grid[i].size(); j++) {
+//             if (grid[i][j] == 1) {
+//                 count++;
+//                 if (i != 0 && grid[i - 1][j] == 1)
+//                     repeat++;
+//                 if (j != 0 && grid[i][j - 1] == 1)
+//                     repeat++;
+//             }
+//         }
+//     }
+//     return 4 * count - repeat * 2;
+// }
+
+// Other ideas (Ref python)
+// Since there are no lakes, every pair of neighbour cells with different values
+// is part of the perimeter (more precisely, the edge between them is). So just
+// count the differing pairs, both horizontally and vertically (for the latter I
+// simply transpose the grid).
+//
+// def islandPerimeter(self, grid):
+//     return sum(sum(map(operator.ne, [0] + row, row + [0]))
+//                for row in grid + map(list, zip(*grid)))
