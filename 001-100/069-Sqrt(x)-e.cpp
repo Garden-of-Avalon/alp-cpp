48fc7806507a4ef101da41b59628638acd618ad4
ys <ys@ysmiles.com>
2017-12-23 23:16:00 -0800

    change unnecessary optimization

--- 001-100/069-Sqrt(x)-e.cpp
+++ 001-100/069-Sqrt(x)-e.cpp
@@ -7,7 +7,7 @@ class Solution {
     int mySqrt(int x) {
         long r = x;
         while (r * r > x)
-            r = (r + x / r) >> 1;
+            r = (r + x / r) / 2;
         return r;
     }
 };
