830e7c4fcc092a14e4b112c5027c6140929d5bc4
ys <ys@ysmiles.com>
2018-02-20 21:04:49 -0800

    695 max area of island

--- 601-700/695-Max_Area_of_Island-e.cpp
+++ 601-700/695-Max_Area_of_Island-e.cpp
@@ -27,4 +27,25 @@
 
 // Note: The length of each dimension in the given grid does not exceed 50.
 
+// dfs
+class Solution {
+    int dfs(vector<vector<int>> &g, int i, int j) {
+        if (i < 0 || i == g.size() || j < 0 || j == g[0].size() || !g[i][j])
+            return 0;
+        g[i][j] = 0;
+        return 1 + dfs(g, i, j - 1) + dfs(g, i, j + 1) + dfs(g, i - 1, j) +
+               dfs(g, i + 1, j);
+    }
 
+  public:
+    int maxAreaOfIsland(vector<vector<int>> &grid) {
+        int ret = 0;
+
+        for (int i = 0; i < grid.size(); ++i)
+            for (int j = 0; j < grid[0].size(); ++j)
+                if (grid[i][j])
+                    ret = max(ret, dfs(grid, i, j));
+
+        return ret;
+    }
+};
