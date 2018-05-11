ea8831ac37c825e86a36f664fdcee2d88da81f4e
ysmiles <ys@ysmiles.com>
2018-05-10 18:14:23 -0700

    506 relative ranks

---
+++ 501-600/506-Relative_Ranks-e.cpp
@@ -0,0 +1,36 @@
+// Given scores of N athletes, find their relative ranks and the people with the
+// top three highest scores, who will be awarded medals: "Gold Medal", "Silver
+// Medal" and "Bronze Medal".
+
+// Example 1:
+// Input: [5, 4, 3, 2, 1]
+// Output: ["Gold Medal", "Silver Medal", "Bronze Medal", "4", "5"]
+// Explanation: The first three athletes got the top three highest scores, so
+// they got "Gold Medal", "Silver Medal" and "Bronze Medal". For the left two
+// athletes, you just need to output their relative ranks according to their
+// scores. Note: N is a positive integer and won't exceed 10,000. All the scores
+// of athletes are guaranteed to be unique.
+
+class Solution {
+  public:
+    vector<string> findRelativeRanks(vector<int> &nums) {
+        map<int, int> score_idx; // score, id
+        for (int i = 0; i < nums.size(); ++i)
+            score_idx.emplace(nums[i], i);
+
+        vector<string> ret(nums.size());
+
+        const vector<string> prize = {"Gold Medal", "Silver Medal",
+                                      "Bronze Medal"};
+        auto getStrByRank = [&](const int &rank) {
+            return rank < 4 ? prize[rank - 1] : to_string(rank);
+        };
+
+        int rank = 1;
+        for (auto rit = score_idx.rbegin(); rit != score_idx.rend();
+             ++rit, ++rank)
+            ret[rit->second] = getStrByRank(rank);
+
+        return ret;
+    }
+};
