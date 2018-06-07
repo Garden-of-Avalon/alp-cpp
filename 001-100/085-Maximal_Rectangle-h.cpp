3f1d2fac1a5276518a4255647f1cc50610cd304a
ys-pc-win <ys@ysmiles.com>
2018-06-07 16:41:54 -0700

    optimize codes

--- 001-100/085-Maximal_Rectangle-h.cpp
+++ 001-100/085-Maximal_Rectangle-h.cpp
@@ -10,37 +10,29 @@
 class Solution {
   public:
     int maximalRectangle(vector<vector<char>> &matrix) {
-        if (!matrix.size())
+        if (matrix.empty() || matrix[0].empty())
             return 0;
         int h = matrix.size(), w = matrix[0].size();
 
-        // row width, height
+        // use w + 1 for convenience of last compare
         vector<int> ht(w + 1, 0);
-        // use w + 1 is for convenience of last compare
-        // see Mark#1
 
         int ret = 0;
         for (int i = 0; i < h; ++i) {
-            stack<int> rIndexes;
+            stack<int> cols;
             for (int j = 0; j < w; ++j)
-                if (matrix[i][j] == '1')
-                    ++ht[j];
-                else
-                    ht[j] = 0;
+                matrix[i][j] == '0' ? ht[j] = 0 : ++ht[j];
 
             for (int j = 0; j < w + 1; ++j) {
-                // Mark#1
-                // for height 1 2 3 4 5, h[j]==0
-                // calculate and compare 5×1, 4×2, 3×3, 2×4, 1×5
-                while (!rIndexes.empty() && ht[j] < ht[rIndexes.top()]) {
-                    int pretop = rIndexes.top();
-                    rIndexes.pop();
-                    ret = max(ret,
-                              ht[pretop] * (rIndexes.empty()
-                                                ? j
-                                                : j - rIndexes.top() - 1));
+                // e.g. ht 1 2 3 4 5 0, ht[j] == 0 (j == 5)
+                // compare 5×1, 4×2, 3×3, 2×4, 1×5
+                while (!cols.empty() && ht[j] < ht[cols.top()]) {
+                    int prev = cols.top();
+                    cols.pop();
+                    ret = max(ret, ht[prev] *
+                                       (cols.empty() ? j : j - cols.top() - 1));
                 }
-                rIndexes.push(j);
+                cols.push(j);
             }
         }
 
