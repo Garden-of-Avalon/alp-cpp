faf9219a572681c150a7587eedfdce2761532d68
ys-pc-win <ys@ysmiles.com>
2018-06-07 19:53:00 -0700

    use unordered set

--- 201-300/217-Contains_Duplicate-e.cpp
+++ 201-300/217-Contains_Duplicate-e.cpp
@@ -5,6 +5,17 @@
 class Solution {
   public:
     bool containsDuplicate(vector<int> &nums) {
+        unordered_set<int> record;
+        for (auto &&n : nums)
+            if (!record.insert(n).second)
+                return true;
+        return false;
+    }
+};
+
+class Solution {
+  public:
+    bool containsDuplicate(vector<int> &nums) {
         return set<int>(nums.begin(), nums.end()).size() < nums.size();
     }
 }
