5ef1f9f0db83d3d34afea7047ec7a8e97a9526f2
ys-pc-win <ys@ysmiles.com>
2018-06-07 17:18:10 -0700

    update an example and comment about switch

--- 101-200/150-Evaluate_Reverse_Polish_Notation-m.cpp
+++ 101-200/150-Evaluate_Reverse_Polish_Notation-m.cpp
@@ -4,7 +4,11 @@
 // Some examples:
 //   ["2", "1", "+", "3", "*"] -> ((2 + 1) * 3) -> 9
 //   ["4", "13", "5", "/", "+"] -> (4 + (13 / 5)) -> 6
+//   ["10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+"]
+//      -> ((10 * (6 / ((9 + 3) * -11))) + 17) + 5 -> 22
 
+// cannot use switch
+// the condition of switch requires integral or enumeration type
 class Solution {
   public:
     int evalRPN(vector<string> &tokens) {
