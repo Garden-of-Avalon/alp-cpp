f54b35b92e380a3955d417e2067db4a942cd8f0b
ys-pc-win <ys@ysmiles.com>
2018-06-07 20:31:14 -0700

    update consise version

--- 101-200/139-Word_Break-m.cpp
+++ 101-200/139-Word_Break-m.cpp
@@ -10,6 +10,26 @@
 // The wordDict parameter had been changed to a list of strings (instead of a
 // set of strings). Please reload the code definition to get the latest changes.
 
+// consise version
+class Solution {
+  public:
+    bool wordBreak(string s, vector<string> &wordDict) {
+        unordered_set<string> dict(wordDict.begin(), wordDict.end());
+
+        vector<bool> dp(s.size() + 1, false);
+        dp[0] = true;
+
+        for (int i = 1; i <= s.size(); ++i)
+            for (int j = i - 1; j >= 0; --j)
+                if (dp[j] && dict.count(s.substr(j, i - j))) {
+                    dp[i] = true;
+                    break;
+                }
+
+        return dp[s.size()];
+    }
+};
+
 // DP
 class Solution {
   public:
