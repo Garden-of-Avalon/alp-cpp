cc4a7a1e32e55d06c124dab1078735139d3217b1
ys-pc <ys@ysmiles.com>
2018-09-09 22:08:28 -0700

    278 optimized ver

--- 201-300/278-First_Bad_Version-e.cpp
+++ 201-300/278-First_Bad_Version-e.cpp
@@ -35,3 +35,24 @@ class Solution {
         return i;
     }
 };
+
+// Optimized ver.
+// Forward declaration of isBadVersion API.
+bool isBadVersion(int version);
+
+class Solution {
+  public:
+    int firstBadVersion(int n) {
+        int l = 1, r = n;
+
+        while (l < r) {
+            int mid = l + (r - l) / 2;
+            if (isBadVersion(mid))
+                r = mid;
+            else
+                l = mid + 1;
+        }
+
+        return l;
+    }
+};
