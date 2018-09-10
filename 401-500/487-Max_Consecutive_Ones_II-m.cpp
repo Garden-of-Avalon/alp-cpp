c34747a9534fdde52719b4a6953a636f5b8a11d6
ys-pc <ys@ysmiles.com>
2018-09-09 20:44:23 -0700

    487. Max Consecutive Ones II

---
+++ 401-500/487-Max_Consecutive_Ones_II-m.cpp
@@ -0,0 +1,40 @@
+// Given a binary array, find the maximum number of consecutive 1s in this array
+// if you can flip at most one 0.
+
+// Example 1:
+// Input: [1,0,1,1,0]
+// Output: 4
+// Explanation: Flip the first zero will get the the maximum number of
+// consecutive 1s.
+//     After flipping, the maximum number of consecutive 1s is 4.
+// Note:
+
+// The input array will only contain 0 and 1.
+// The length of input array is a positive integer and will not exceed 10,000
+// Follow up:
+// What if the input numbers come in one by one as an infinite stream? In other
+// words, you can't store all numbers coming from the stream as it's too large
+// to hold in memory. Could you solve it efficiently?
+
+class Solution {
+  public:
+    int findMaxConsecutiveOnes(vector<int> &nums) {
+        int ret = 0, cnt = 0;
+
+        for (int i = 0, j = 0; j < nums.size(); ++j) {
+            if (!nums[j])
+                ++cnt;
+
+            if (cnt == 2) {
+                ret = max(ret, j - i);
+                // make the substr i to j (included) contain only one 0
+                while (nums[i++])
+                    ;
+                cnt = 1;
+            } else
+                ret = max(ret, j - i + 1);
+        }
+
+        return ret;
+    }
+};
