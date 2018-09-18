e18a151ace2f2547559f0bf4dbbd2394f45f6258
ys-pc <ys@ysmiles.com>
2018-09-18 00:10:20 -0700

    update the comments to help understand better

--- TaoLu/heap_sort.cpp
+++ TaoLu/heap_sort.cpp
@@ -4,28 +4,32 @@
 using namespace std;
 
 template <typename T> class HeapSort {
-    // make A[root] the max value of the subtree
+    // make A[root-end) a heap
     void heapify(vector<T> &A, int root, int end) {
-        int max_val_idx = root;
         int left = 2 * root + 1, right = left + 1;
 
-        if (left < end && A[left] > A[max_val_idx])
-            max_val_idx = left;
-        if (right < end && A[right] > A[max_val_idx])
-            max_val_idx = right;
+        int max_of_three = root; // max of root and its childs
+        if (left < end && A[left] > A[max_of_three])
+            max_of_three = left;
+        if (right < end && A[right] > A[max_of_three])
+            max_of_three = right;
 
-        if (max_val_idx != root) {
-            swap(A[root], A[max_val_idx]);
-            heapify(A, max_val_idx, end);
+        if (max_of_three != root) {
+            swap(A[root], A[max_of_three]);
+            heapify(A, max_of_three, end);
         }
     }
 
   public:
     void hs(vector<T> &A) {
-        for(int i = A.size() / 2 - 1; i >= 0; --i) 
+        // pre-build whole vector as heap, O(n)
+        for (int i = A.size() / 2 - 1; i >= 0; --i)
             heapify(A, i, A.size());
-        
-        for(int i = A.size() - 1; i >= 0; --i) {
+
+        // O(nlogn)
+        // for each step:
+        // select max, move down root to correct position
+        for (int i = A.size() - 1; i >= 0; --i) {
             swap(A[0], A[i]);
             heapify(A, 0, i);
         }
@@ -36,6 +40,7 @@ int main() {
     vector<int> unsorted = {3, 2, 1, 5, 9, 7, 4};
     HeapSort<int> sorter;
     sorter.hs(unsorted);
+
     for (auto x : unsorted)
         cout << x << ' ';
     cout << endl;
